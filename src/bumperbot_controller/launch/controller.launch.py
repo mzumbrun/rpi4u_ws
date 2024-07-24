64f22217fd41320e9d4893d3842fee131a0c705e
mzumbrun <m.l.zumbrunnen@gmail.com>
2024-07-24 15:27:48 -0500

    iteration thru 7-24-24

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -49,19 +49,19 @@ def generate_launch_description():
     )
     wheel_radius_arg = DeclareLaunchArgument(
         "wheel_radius",
-        default_value="0.033",
+        default_value="0.063",
     )
     wheel_separation_arg = DeclareLaunchArgument(
         "wheel_separation",
-        default_value="0.17",
+        default_value="0.438",
     )
     wheel_radius_error_arg = DeclareLaunchArgument(
         "wheel_radius_error",
-        default_value="0.005",
+        default_value="0.001",
     )
     wheel_separation_error_arg = DeclareLaunchArgument(
         "wheel_separation_error",
-        default_value="0.02",
+        default_value="0.001",
     )
     
     use_simple_controller = LaunchConfiguration("use_simple_controller")
