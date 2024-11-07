cc83ef1fcceb24b987ff8345cd2491ee81ea7f92
mzumbrun <m.l.zumbrunnen@gmail.com>
2024-11-07 15:14:58 -0600

    first attempt ekf frame id changes

--- src/mybots_localization/launch/local_localization.launch.py
+++ src/mybots_localization/launch/local_localization.launch.py
@@ -20,8 +20,8 @@ def generate_launch_description():
         executable="static_transform_publisher",
         arguments=["--x", "0", "--y", "0","--z", "0.103",
                    "--qx", "1", "--qy", "0", "--qz", "0", "--qw", "0",
-                   "--frame-id", "base_footprint_ekf",
-                   "--child-frame-id", "imu_link_ekf"],
+                   "--frame-id", "base_footprint", # MLZ was base_footprint_ekf
+                   "--child-frame-id", "imu_link"],# was imu_link_ekf
     )
 
     robot_localization = Node(
